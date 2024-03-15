30909333e0a9e238a29ffe0b6c3408d7d8e44899
Justin P <161922331+justinpongos@users.noreply.github.com>
2024-03-14 20:55:45 -0700

    Add files via upload

---
+++ TriArea.java
@@ -0,0 +1,18 @@
+
+public class TriArea {
+	public static void main(String[]args) {
+		
+		//assigned variables b & h (pos int values)
+		int base = 3;
+		int height = 6;
+		double x = 0.5;
+		//b & h values printed
+		System.out.println("value of b: " + base);
+		System.out.println("value of h: " + height);
+		//the value of "1/2 *b" printed
+		System.out.println("value of 1/2*b: " + x * base);
+		//the area of the triangle printed
+		System.out.println("value of A: " + x * base * height);
+
+	}//end main
+}//end class
